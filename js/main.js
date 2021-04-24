2ecb8be0d4cef9f0820d88e929a3a3c1887a0cc3
akhil-hari <aklhnrpm@gmail.com>
2021-04-24 21:36:31 +0530

    935p240421 commit

--- js/main.js
+++ js/main.js
@@ -35,21 +35,29 @@ function load_list(){
             e.innerHTML="<i>no tasks to show.</i>"
         }
     };
-    res.open("GET","js/todos.json",true);
+    res.open("GET","https://jsonplaceholder.typicode.com/todos",true);
     res.send(null);
     
 
 }
-load_list();
+// load_list();
 // setInterval(load_list,60000);
 
 function checkbox_change(){
     if((document.querySelectorAll('.task:checked').length-countg)==5){
         bootbox.alert({message:"<span style='position:relative;left:20px;top:15px;border-radius:50%;float:left'><img height='160px' width='160px' src='./res/tenor3.gif' style='border-radius:50%;float:left'></img><h1 style='color:rgba(102,113,134,255);position:relative;left:-60px;top:60px;float:right;margin-left:30px'>Congratz.!</h1><small style='color:rgb(244,180,100);position:relative;top:60px;left:20px;white-space:nowrap'>you have compleated 5 tasks today.</small></span>",centerVertical:true});
-    }
-
-    
+    }    
 };
+
+function loginbtn(){
+    if((document.querySelector("#uname").value=="admin")&&(document.querySelector("#pword").value=="12345")){
+        console.log("login sucks");
+        window.location.href="./todo.html"
+    }
+    else{
+        document.querySelector("#warn").innerHTML="Username or Password is incorrect.";
+    }
+}
 
 // Array.from(document.getElementsByClassName('task')).forEach((element) => {
 //         element.addEventListener('change',()=> {
